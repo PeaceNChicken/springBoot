7fc56201e1186bb42c9c6fcd478d1643d52af0ae
PeaceNChicken <kkjlevel33@gmail.com>
2019-07-26 17:17:21 +0900

    연습용예제

---
+++ ksmart_layout3/src/main/java/kr/or/ksmart/ksmart_DAO/Member.java
@@ -0,0 +1,9 @@
+package kr.or.ksmart.ksmart_DAO;
+
+public class Member {
+	String memberId;
+	String memberPw;
+	String memberLevel;
+	String memberName;
+	String memberEmail;
+}
---
+++ ksmart_layout3/src/main/java/kr/or/ksmart/ksmart_layout/KsmartLayoutApplication.java
@@ -0,0 +1,13 @@
+package kr.or.ksmart.ksmart_layout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsmartLayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsmartLayoutApplication.class, args);
+	}
+
+}
